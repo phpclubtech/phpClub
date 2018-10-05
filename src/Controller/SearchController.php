53da6672474f9fdd5fad144ac7db4bf247ec5158
someApprentice <someApprentice@protonmail.com>
2018-10-05 16:26:54 +0700

    refixed xss vulnerability

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -65,7 +65,7 @@ class SearchController
 
     public function searchAction(Request $request, Response $response): ResponseInterface
     {
-        $query = \htmlspecialchars($request->getParam('q'), ENT_QUOTES, 'UTF-8');
+        $query = $request->getParam('q');
         $page = $request->getParam('page', 1);
 
         $posts = (new Pagerfanta(new SphinxAdapter($this->sphinxConnection, $this->postRepository, $query)))
